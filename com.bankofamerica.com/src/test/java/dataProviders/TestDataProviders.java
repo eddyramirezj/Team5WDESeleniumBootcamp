140e8b929763b6accbcc4eac187bc428a1a5b121
“Salma” <h.n222@yahoo.com>
2021-10-11 22:28:24 -0400

    3 data driven test cases added in BOA app

--- com.bankofamerica.com/src/test/java/dataProviders/TestDataProviders.java
+++ com.bankofamerica.com/src/test/java/dataProviders/TestDataProviders.java
@@ -18,4 +18,23 @@ public class TestDataProviders {
         return testData;
     }
 
+    @DataProvider(name="BoaData")
+    public Object[][] getLoginData(){
+
+        Object[][] obj = new Object[3][2];
+
+        obj[0][0] = "abcde";
+        obj[0][1] = "1234#";
+
+        obj[1][0] = "fghij";
+        obj[1][1] = "5678#";
+
+        obj[2][0] = "pqrst";
+        obj[2][1] = "98765#";
+
+
+        return obj;
+    }
+
+
 }
--- com.bankofamerica.com/src/test/java/testBoaPom/TestBoaHomePage.java
+++ com.bankofamerica.com/src/test/java/testBoaPom/TestBoaHomePage.java
@@ -1,19 +1,65 @@
 package testBoaPom;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import testBase.TestBase;
 
 public class TestBoaHomePage extends TestBase {
 
-
-@Test
-    public void testBoaLogin(){
+    @Test(enabled = true)
+    public void testBoaLogin1(){
 
     BoaHomePage boaHomePage = new BoaHomePage();
-    boaHomePage.testLogin("abcde", "12345#");
-
+    boaHomePage.testLogin("WXYZ", "12345#");
 
 }
+
+
+
+
+    @DataProvider(name="BoaData")
+    public Object[][] getLoginData(){
+
+        Object[][] obj = new Object[3][2];
+
+        obj[0][0] = "ABCDE";
+        obj[0][1] = "1234#";
+
+        obj[1][0] = "FGHIJK";
+        obj[1][1] = "5678#";
+
+        obj[2][0] = "LMNOP";
+        obj[2][1] = "98765#";
+
+
+        return obj;
+    }
+
+    @Test(enabled = true, dataProvider = "BoaData")
+    public void testBoaLogin2(String s1, String s2){
+
+        BoaHomePage boaHomePage = new BoaHomePage();
+        boaHomePage.testLogin(s1,s2);
+
+    }
+
+    @Test(enabled = true, dataProvider = "BoaData", dataProviderClass = dataProviders.TestDataProviders.class)
+    public void testBoaLogin3(String s1, String s2){
+
+        BoaHomePage boaHomePage = new BoaHomePage();
+        boaHomePage.testLogin(s1,s2);
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 }
